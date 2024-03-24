5e1787c94e17da0c70bf9a4336445940fec45ba9
nihad <mernnihadsoft@gmail.com>
2024-03-25 01:06:03 +0530

    builded admin

--- src/api/instance.js
+++ src/api/instance.js
@@ -4,5 +4,9 @@ import axios from 'axios';
 const API_BASE_URL = 'https://www.propertyseller.ae/api/v1';
 export const api = axios.create({
   withCredentials: true,
+  headers: { 'Content-Type': 'application/json'},
   baseURL: API_BASE_URL,
 });
+
+api.defaults.headers.common['Access-Control-Allow-Origin'] = '*'
+api.defaults.headers.common['Access-Control-Allow-Methods'] = 'GET,POST,PUT,DELETE'
